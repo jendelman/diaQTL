fe5fd5b3bb5d4bea8089a5365d4348facfb79be6
Jeffrey Endelman <endelman@wisc.edu>
2020-12-29 10:51:03 -0500

    v0.90

--- R/fitQTL.R
+++ R/fitQTL.R
@@ -2,7 +2,7 @@
 #' 
 #' Fit a single QTL model
 #' 
-#' LOD score is the difference between the log10-likelihood for the QTL model vs. no QTL model (higher is better). deltaDIC is the difference between the Deviance Information Criterion for the QTL model vs. no QTL model (lower values is better). r2 is the squared correlation between the fitted and observed values. Parameter \code{dominance} controls the genetic model: 1 = additive, 2 = digenic dominance, 3 = trigenic dominance, 4 = quadrigenic dominance. MCMC \code{params} can be estimated using \code{\link{set_params}}. Parameter \code{CI.prob} sets the probability (e.g., 0.90, 0.95) for the Bayesian credible interval for the estimated effects. The returned list \code{effects} contains the additive (and when included) digenic dominance effects. The proportion of variance for each effect is returned in \code{var}. The returned object \code{plots$dom} shows the digenic dominance effects above the diagonal, and below the diagonal is the sum of the additive and digenic dominance effects. The polygenic background effect has covariance equal to the additive relationship computed by \code{\link{IBDmat}}, leaving out the chromosome with the QTL. For faster execution with the polygenic model, use \code{\link{Gprep}} first.
+#' LOD score is the difference between the log10-likelihood for the QTL model vs. no QTL model (higher is better). deltaDIC is the difference between the Deviance Information Criterion for the QTL model vs. no QTL model (lower values is better). r2 is the squared correlation between the fitted and observed values. Parameter \code{dominance} controls the genetic model: 1 = additive, 2 = digenic dominance, 3 = trigenic dominance, 4 = quadrigenic dominance. MCMC \code{params} can be estimated using \code{\link{set_params}}. Parameter \code{CI.prob} sets the probability (e.g., 0.90, 0.95) for the Bayesian credible interval for the estimated effects (to disable plotting of the CI, use \code{CI.prob=NULL}). The returned list \code{effects} contains the additive (and when included) digenic dominance effects. The proportion of variance for each effect is returned in \code{var}. The returned object \code{plots$dom} shows the digenic dominance effects above the diagonal, and below the diagonal is the sum of the additive and digenic dominance effects. The polygenic background effect has covariance equal to the additive relationship computed by \code{\link{IBDmat}}, leaving out the chromosome with the QTL. For faster execution with the polygenic model, use \code{\link{Gprep}} first.
 #' 
 #' @param data Variable of class \code{\link{diallel_geno_pheno}}
 #' @param trait Name of trait
@@ -102,26 +102,41 @@ fitQTL <- function(data,trait,marker,params,dominance=1,CI.prob=0.9,polygenic=TR
 
   for (j in 1:dominance) {
     ans <- readBinMat(sub(pattern="X",replacement=j,x="tmp/ETA_aX_b.bin"))
+    ans <- t(apply(ans,1,function(z){z-mean(z)})) #enforce constraint that sum of effects is zero
     effect.mean[[j]] <- apply(ans,2,mean)
-    tmp <- apply(ans,2,quantile,p=c(0.5-CI.prob/2,0.5+CI.prob/2))
-    effect.lower[[j]] <- tmp[1,]
-    effect.upper[[j]] <- tmp[2,]
+    if (!is.null(CI.prob)) {
+      tmp <- apply(ans,2,quantile,p=c(0.5-CI.prob/2,0.5+CI.prob/2))
+      effect.lower[[j]] <- tmp[1,]
+      effect.upper[[j]] <- tmp[2,]
+    }
     variances[,as.integer(polygenic)+j] <- apply(tcrossprod(data@Z %*% data@geno[[marker2]][[j]],ans),2,var)
   }
   varE <- scan("tmp/varE.dat",quiet = T)[params$burnIn+1:(params$nIter-params$burnIn)]
   h2 <- variances/(apply(variances,1,sum) + varE)  #proportion of variance for each term
-  tmp <- apply(h2,2,quantile,p=c(0.5-CI.prob/2,0.5+CI.prob/2))
-  return.var <- cbind(Mean=round(apply(h2,2,mean),2),CI.lower=round(tmp[1,],2),CI.upper=round(tmp[2,],2))
+  if (!is.null(CI.prob)) {
+    tmp <- apply(h2,2,quantile,p=c(0.5-CI.prob/2,0.5+CI.prob/2))
+    return.var <- cbind(Mean=round(apply(h2,2,mean),2),CI.lower=round(tmp[1,],2),CI.upper=round(tmp[2,],2))
+  } else {
+    return.var <- cbind(Mean=round(apply(h2,2,mean),2))
+  }
 
   effects <- vector("list",length=ifelse(dominance==1,1,2))
   names(effects) <- c("add","dom")[1:length(effects)]
 
-  effects$add <- data.frame(Haplotype=attr(data@geno,"haplotypes"),Mean=effect.mean[[1]],CI.lower=effect.lower[[1]],CI.upper=effect.upper[[1]],stringsAsFactors = F)
+  if (!is.null(CI.prob)) {
+    effects$add <- data.frame(Haplotype=attr(data@geno,"haplotypes"),Mean=effect.mean[[1]],CI.lower=effect.lower[[1]],CI.upper=effect.upper[[1]],stringsAsFactors = F)
+  } else {
+    effects$add <- data.frame(Haplotype=attr(data@geno,"haplotypes"),Mean=effect.mean[[1]],stringsAsFactors = F)
+  }
   effects$add <- effects$add[order(effects$add$Haplotype),]
 
   if (dominance > 1) {
     diplotypes <- strsplit(attr(data@geno,"diplotypes"),split="+",fixed=T)
-    effects$dom <- data.frame(Haplotype1=sapply(diplotypes,function(x){x[1]}),Haplotype2=sapply(diplotypes,function(x){x[2]}),Mean=effect.mean[[2]],CI.lower=effect.lower[[2]],CI.upper=effect.upper[[2]],stringsAsFactors = F)
+    if (!is.null(CI.prob)) {
+      effects$dom <- data.frame(Haplotype1=sapply(diplotypes,function(x){x[1]}),Haplotype2=sapply(diplotypes,function(x){x[2]}),Mean=effect.mean[[2]],CI.lower=effect.lower[[2]],CI.upper=effect.upper[[2]],stringsAsFactors = F)
+    } else {
+      effects$dom <- data.frame(Haplotype1=sapply(diplotypes,function(x){x[1]}),Haplotype2=sapply(diplotypes,function(x){x[2]}),Mean=effect.mean[[2]],stringsAsFactors = F)
+    }
     effects$dom <- effects$dom[order(effects$dom$Haplotype1,effects$dom$Haplotype2),]
   }
 
@@ -139,8 +154,10 @@ fitQTL <- function(data,trait,marker,params,dominance=1,CI.prob=0.9,polygenic=TR
     geom_bar(stat="identity",position="dodge",colour="black",fill="grey50") + 
     ylab("Additive Effect") + 
     theme(text = element_text(size=13)) + 
-    facet_grid(.~founders,scales = "free_x") +
-    geom_errorbar(aes(ymax=CI.upper, ymin=CI.lower, width = 0.2))
+    facet_grid(.~founders,scales = "free_x")
+  if (!is.null(CI.prob)) {
+    plotA <- plotA + geom_errorbar(aes(ymax=CI.upper, ymin=CI.lower, width = 0.2))
+  }
   
   if (dominance > 1) {
     #Digenic dominance effects
