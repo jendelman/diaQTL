55a60d3d0732ab59745f804e961583882f589132
jendelman <endelman@wisc.edu>
2025-12-02 11:54:54 -0600

    v1.11

--- R/BayesCI.R
+++ R/BayesCI.R
@@ -10,7 +10,7 @@
 #' @param chrom chromosome
 #' @param CI.prob probability for the credible interval
 #' 
-#' @return subset of scan1_data with markers in the CI
+#' @return first and last marker in the CI
 #'
 #' @examples
 #' \dontrun{
@@ -25,7 +25,9 @@ BayesCI <- function(scan1_data,data,chrom,CI.prob=0.9) {
   ix <- which(scan1a$chrom == chrom)
   x <- scan1a$cM[ix]
 
-  tmp <- data.frame(pos=ix,left=c(0,diff(x)/2),right=c(diff(x)/2,0),prob=exp(scan1a$LL[ix]))
+  tmp <- data.frame(pos=ix,left=c(0,diff(x)/2),
+                    right=c(diff(x)/2,0),
+                    prob=exp(scan1a$LL[ix]-mean(scan1a$LL[ix])))
 
   tmp$y <- tmp$prob*(tmp$right+tmp$left)
   tmp$area <- tmp$y/sum(tmp$y)
@@ -38,5 +40,5 @@ BayesCI <- function(scan1_data,data,chrom,CI.prob=0.9) {
   upper <- min(upper+1,n)
   limits <- scan1a$cM[ix[c(lower,upper)]]
   out <- scan1_data[scan1_data$chrom == chrom & scan1_data$cM >= limits[1] & scan1_data$cM <= limits[2],]
-  return(out)
+  return(out[c(1,nrow(out)),])
 }
